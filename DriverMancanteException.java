b525b69f81cea09aa18db51b6867a469c7b434f6
KamiDaga <95750384+KamiDaga@users.noreply.github.com>
2021-12-20 07:56:36 +0100

    Tolta la "è" nella prima frase del MessageDialog

--- DriverMancanteException.java
+++ DriverMancanteException.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class DriverMancanteException extends ErroreIniziale {
 
 	public void StampaMessaggio() {
-		JOptionPane.showMessageDialog(null, "C'è stato un errore, il driver del DBMS potrebbe"
+		JOptionPane.showMessageDialog(null, "C'e' stato un errore, il driver del DBMS potrebbe"
 				+ " non essere stato installato correttamente.\n"
 				+ "Riprovare l'installazione"
 				+ " e riavviare l'applicativo.", "Errore!", JOptionPane.ERROR_MESSAGE);
