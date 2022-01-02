aa528b933b35477492a1e446baeeedc06dd9f32a
FerdiDale <95748075+FerdiDale@users.noreply.github.com>
2022-01-02 20:28:38 +0100

    Update SalaDAO.java

--- SalaDAO.java
+++ SalaDAO.java
@@ -7,6 +7,4 @@ public interface SalaDAO
 	public void RimuoviSalaRistorante(Sala s);
 	
 	public void AggiuntaSalaRistorante(String nomeSala, int id_ristorante);
-	
-	public boolean isNomeSalaTaken(String nomeSala, int id_ristorante);
 }
