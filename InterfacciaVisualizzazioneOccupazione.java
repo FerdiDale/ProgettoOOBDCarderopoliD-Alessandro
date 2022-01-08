4cc344fe3062b7b791b2c1d99cd3a252f32739f9
FerdiDale <95748075+FerdiDale@users.noreply.github.com>
2022-01-08 10:13:20 +0100

    Update InterfacciaVisualizzazioneOccupazione.java

--- InterfacciaVisualizzazioneOccupazione.java
+++ InterfacciaVisualizzazioneOccupazione.java
@@ -89,7 +89,7 @@ public class InterfacciaVisualizzazioneOccupazione extends JFrame {
 		bottoneRimuoviAvventore.setBounds(10, 190, 170, 23);
 		getContentPane().add(bottoneRimuoviAvventore);
 		bottoneRimuoviAvventore.addActionListener(new GestioneBottoni());
-	
+		bottoneRimuoviAvventore.setEnabled(false);
 		
 		bottoneIndietro = new JButton("Indietro");
 		bottoneIndietro.setBounds(10, 257, 89, 23);
@@ -212,7 +212,7 @@ public class InterfacciaVisualizzazioneOccupazione extends JFrame {
 			if(!arrayAvventori.get(indiceListaAvventori!=-1 ? indiceListaAvventori : 0).getN_tel().isBlank() && contaNTel == 1) 
 				{
 					bottoneRimuoviAvventore.setEnabled(false);
-					bottoneRimuoviAvventore.setToolTipText("Non si può rimuovere un avventore con un numero di telefono!");
+					bottoneRimuoviAvventore.setToolTipText("Non si puÃ² rimuovere un avventore con un numero di telefono!");
 				}
 			if(tavoli.get(tavoloScelto).getCapacita() == arrayAvventori.size()) 
 				{
